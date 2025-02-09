d35177c46f7e984a8ede0c490d3f219e2a21fa3f
AshotHeb <heboyanashot@gmail.com>
2025-02-09 15:26:15 +0400

    @Angular Basics/lesson-2 -- component

---
+++ src/app/first-component/first-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrl: './first-component.component.scss',
+})
+export class FirstComponentComponent {}
