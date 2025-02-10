31e47795cd966061e90ef23f6b3590b992454215
AshotHeb <heboyanashot@gmail.com>
2025-02-10 14:27:41 +0400

    @Angular Basics/lesson-2 -- two way binding/ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { EventListenersComponent } from './event-listeners/event-listeners.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponentComponent, EventListenersComponent],
+  imports: [
+    RouterOutlet,
+    FirstComponentComponent,
+    EventListenersComponent,
+    TwoWayBindingComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
