8c7b8637fa1d13302e900bed67981086e6f13c43
AshotHeb <heboyanashot@gmail.com>
2025-02-17 23:35:20 +0400

    @Angular Basics/lesson3/components -- Lifecycle methods

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,4 +1,11 @@
-import { Component, ContentChild, ElementRef, Input } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { Post } from '../posts.component';
 import { CommonModule } from '@angular/common';
 
@@ -13,8 +20,21 @@ export class PostComponent {
   @Input() post!: Post;
 
   @ContentChild('postMessage') postMessage!: ElementRef;
+  @Output() deletePost = new EventEmitter<Post>();
 
   ngAfterContentInit() {
-    console.log(this.postMessage.nativeElement);
+    // Երբ կոմպոնենտը ստանում է դրսից content (children in React) և ավարտվում է այդ content-ի նկարվելը DOM-ի մեջ,կանչվում է այս մեթոդը
+    // console.log(this.postMessage.nativeElement);
+    console.log('PostComponent -- ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    // Երբ այդ content-ը update է լինում ,այդ ժամանակ այս մեթոդն է կանչվում
+    console.log('PostComponent --  ngAfterContentChecked');
+  }
+
+  ngOnDestroy() {
+    // Երբ կոմպոնենտը ջնջվում է ,այդ ժամանակ այս մեթոդն է կանչվում
+    console.log('PostComponent --  ngOnDestroy');
   }
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { PostInputComponent } from './post-input/post-input.component';
 import { PostComponent } from './post/post.component';
 import { CommonModule } from '@angular/common';
@@ -15,7 +15,7 @@ export interface Post {
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss',
 })
-export class PostsComponent {
+export class PostsComponent implements OnChanges, OnInit {
   posts: Post[] = [
     {
       title: 'First Post',
@@ -32,5 +32,39 @@ export class PostsComponent {
 
   addPost(post: Post) {
     this.posts.push(post);
+  }
+
+  constructor() {
+    // console.log('LifeCycle Hooks Calls');
+    // console.log('constructor');
+  }
+  deletePost(post: Post) {
+    this.posts = this.posts.filter((p) => p.id !== post.id);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // Կանչվում է դրսի տվյալի փոփոխման դեպքում
+    // Այս կոմպոնենտում չի աշխատի ,քանի որ չունենք դրսից եկող ոչ մի տվյալ
+    // console.log('PostsComponent -- ngOnChanges', changes);
+  }
+
+  ngOnInit() {
+    // Կանչվում է կոմպոնենտի լոգիկական մասը պատրաստ է ,componentDidMount-ի նման React-ի
+    // console.log('PostsComponent -- ngOnInit');
+  }
+
+  ngDoCheck() {
+    // Աշխատում է կոմպոնենտի ամեն մի փոփոխության դեպքում (componentDidUpdate-ի նման React-ի)
+    // console.log('PostsComponent -- ngDoCheck');
+  }
+
+  ngAfterViewInit() {
+    //Աշխատում է երբ արդեն կոմպոնենտը երևւմ է էկրանին
+    // console.log('PostsComponent -- ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    // Երբ կոմպոնենտի տեսանելի մասը update է լինում ,այդ ժամանակ այս մեթոդն է կանչվում
+    // console.log('PostsComponent -- ngAfterViewChecked');
   }
 }
